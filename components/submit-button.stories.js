f5ab41c5d19b65af4802412fb6e66c9e36025091
Melissa Celeste Gries <m.c.gries@gmx.de>
2021-01-22 10:24:44 +0100

    add button styles

---
+++ components/submit-button.stories.js
@@ -0,0 +1,10 @@
+import '../styles/index.css'
+
+import './submit-button.css'
+import navigation from './submit-button.html'
+
+export default {
+  title: 'submit-button',
+}
+
+export const base = () => submit - button
